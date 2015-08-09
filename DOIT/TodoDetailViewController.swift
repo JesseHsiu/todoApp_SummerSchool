fc13f3a80c8147013557024c62287b092045c515
JesseHsiu <jesse801122@gmail.com>
2015-08-09 23:09:47 +0800

    add TodoDetailViewController.swift and add to SB

---
+++ DOIT/TodoDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TodoDetailViewController.swift
+//  DOIT
+//
+//  Created by 修敏傑 on 8/9/15.
+//  Copyright (c) 2015 NTU. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TodoDetailViewController: UIViewController{
+
+}
