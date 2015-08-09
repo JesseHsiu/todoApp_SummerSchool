b0641ef461f7ce8fe86fde68023c2ebf6872cb15
JesseHsiu <jesse801122@gmail.com>
2015-08-09 23:38:51 +0800

    add dataContainer and UITableview's delegate

--- DOIT/ViewController.swift
+++ DOIT/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    var dataContainer = Array(count:4, repeatedValue:[TodoDataClass]())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -26,6 +28,55 @@ class ViewController: UITableViewController {
     func addBtnPressed(){
         let todoVC : TodoDetailViewController = self.storyboard!.instantiateViewControllerWithIdentifier("detail") as! TodoDetailViewController
         self.navigationController!.pushViewController(todoVC, animated: true)
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return dataContainer.count;
+    }
+    
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        switch section
+        {
+            case 0:
+                return typeOfTodo.urgent.rawValue
+            case 1:
+                return typeOfTodo.normal.rawValue
+            case 2:
+                return typeOfTodo.casual.rawValue
+            case 3:
+                return typeOfTodo.done.rawValue
+            default:
+                return typeOfTodo.normal.rawValue
+        }
+
+    }
+    
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return dataContainer[section].count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell: UITableViewCell? = tableView.dequeueReusableCellWithIdentifier("reuse") as? UITableViewCell
+        
+        if cell == nil
+        {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "reuse")
+        }
+        
+        var data = dataContainer[indexPath.section][indexPath.row] as TodoDataClass
+        
+        cell!.textLabel?.text = data.title
+        
+        return cell!
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+
     }
 
 }
